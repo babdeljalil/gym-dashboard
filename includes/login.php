34b286f8ac096680862d939acdbc9716ba232e86
babdeljalil <158838565+babdeljalil@users.noreply.github.com>
2025-07-06 12:21:37 +0100

    Add config.php and improve error handling in login.php
    Added a new config.php file with basic HTML structure. Updated login.php to include database connection within a try-catch block for better error handling when processing POST requests.

--- includes/login.php
+++ includes/login.php
@@ -1,7 +1,13 @@
 <?php 
-include_once 'db.php';
 
 if ($_SERVER["REQUEST_SERVER"] === "POST") {
   $username = $_POST["username"];
   $password = $_POST["password"];
+  
+  try {
+    include_once 'db.php';
+
+  } catch(PDOException $e) {
+    die("Error" . $e->getMessage());
+  }
 }
